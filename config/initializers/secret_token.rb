a5cfd201157d19a287963eead290056bc908fcfe
Gerard Deib <gerarddeib@gmail.com>
2013-11-30 08:58:18 +1300

    Innitial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinty::Application.config.secret_key_base = '380070e8711d173196b46a37cb9606c2da044d674e061d76e7155c5fd9bc378cfdf91efa7c0988fb7a0b1deb087434bdafb36d23b572e389ca969417dcc445d1'
